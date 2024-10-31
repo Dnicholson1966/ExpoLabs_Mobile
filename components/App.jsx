5eb4e67fa62e8f4965d4d247a388c137f69c0905
Derrick Nicholson <106172127+Dnicholson1966@users.noreply.github.com>
2024-10-31 13:58:20 -0600

    Renamed project, and worked on lab 3.

--- components/App.jsx
+++ components/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
 import ToDoList from './ToDoList';
 import ToDoForm from './ToDoForm';
 
 function App() {
+  // Current array of tasks to complete.
+  const currentTasks = [
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ];
+  const [tasks, setTasks ] = useState(currentTasks) // useState hook, takes in current tasks.
+
   return (
     <SafeAreaView>
-      <ToDoList />
+      <ToDoList tasks={tasks} />
       <ToDoForm />
     </SafeAreaView>
   );
